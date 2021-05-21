0c3409e57451d056d792975127e2918b45d73324
Tom Davidson <thomas.davidson3@skybettingandgaming.com>
2021-05-21 21:17:37 +0100

    added usage instructions

--- lab-setup.sh
+++ lab-setup.sh
@@ -1,3 +1,5 @@
+# USAGE: eval "$(curl https://raw.githubusercontent.com/tpd1001/cka-resources/main/lab-setup.sh)"
+# ALTERNATE USAGE: source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/tpd1001/cka-resources/main/lab-setup.sh)"
 source <(kubectl completion bash|sed '/ *complete .*kubectl$/{;h;s/kubectl$/k/p;g;}')
 k() { kubectl $@; }
 v() { vim -c ":set shiftwidth=2 tabstop=2 softtabstop=-1 expandtab" ${1?} && kubectl apply -f $1; }
