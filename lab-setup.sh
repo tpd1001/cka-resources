be5c793ec8dddf76edf171937b8cdc63b57043b7
Tom Davidson <thomas.davidson3@skybettingandgaming.com>
2022-03-10 09:46:13 +0000

    added Mac auto pbcoy

--- lab-setup.sh
+++ lab-setup.sh
@@ -1,5 +1,6 @@
 # USAGE: eval "$(curl https://raw.githubusercontent.com/tpd1001/cka-resources/main/lab-setup.sh)"
 # ALTERNATE USAGE: source /dev/stdin <<< "$(curl https://raw.githubusercontent.com/tpd1001/cka-resources/main/lab-setup.sh)"
+uname|grep -q Darwin && sed -n 's/^# USAGE: eval/eval/p' $0|pbcopy && exit
 source <(kubectl completion bash|sed '/ *complete .*kubectl$/{;h;s/kubectl$/k/p;g;}')
 k() { kubectl $@; }
 v() { vim -c ":set shiftwidth=2 tabstop=2 softtabstop=-1 expandtab" ${1?} && kubectl apply -f $1; }
